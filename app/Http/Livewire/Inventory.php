2ec7426e26d3604e3a197e7cf518095a681fb715
wahyuidiots <wuuuahyou@gmail.com>
2023-04-28 11:20:08 +0800

    pertamax

---
+++ app/Http/Livewire/Inventory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Inventory extends Component
+{
+    public function render()
+    {
+
+        return view('livewire.inventory');
+    }
+}
